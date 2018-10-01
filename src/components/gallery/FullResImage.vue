b5c02abf307792dce566f3114462d374e1934166
Ambrus Attila <attilawow91@gmail.com>
2018-10-01 18:59:24 +0300

    add spinner

--- src/components/gallery/FullResImage.vue
+++ src/components/gallery/FullResImage.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="full-image">
-    <img :src="clickedImage" alt="" class="full-image__img" @load="loadFinished = !loadFinished" v-show="loadFinished">
+    <div class="lds-ring" v-show="!loadFinished">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <img :src="clickedImage" alt="" class="full-image__img" @load="loadFinished = true" v-show="loadFinished">
   </div>
 </template>
 
@@ -29,6 +35,41 @@ export default {
       height: 100%;
       width: 100%;
       object-fit: contain;
+    }
+  }
+  .lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 51px;
+    height: 51px;
+    margin: 6px;
+    border: 6px solid #fff;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #fff transparent transparent transparent;
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
     }
   }
 </style>
